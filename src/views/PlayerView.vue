5e4ee9a8fbd4056dcf4197f17422508c8d23ce43
Nikki <ngolev.bus@gmail.com>
2024-03-05 18:45:37 +1100

    edit avatar

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -6,6 +6,10 @@
                 <div class="text-center">
                     <img :src="$md.ply.render.avatar" class="img-fluid shadow rounded d-none d-md-block mx-auto"
                         style="max-height: 500px;" />
+                    <div v-if="$md.ply.render.avatar"
+                        :style="{ backgroundImage: 'url(' + $md.ply.render.avatar + ')', backgroundSize: 'cover' }"
+                        style="height: 250px; width: 250px; border-radius: 50%;"
+                        class="img-fluid border border-light shadow d-block d-md-none mx-auto"></div>
                 </div>
                 <PlayerBio />
             </div>

8e6fe440db4cf94fdc7a9af7f3b72a82b0802cf9
Nikita Golev <ngolev.bus@gmail.com>
2025-09-16 14:01:23 +1000

    add new profile image and update AboutView content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,24 +5,25 @@
                 <i class="bi bi-arrow-left"></i> Back
             </router-link>
         </div>
-        <div class="text-center">
+        <div class="text-center text-md-start">
             <h1 class="display-1 mb-0 col text-nowrap">Magic Dice</h1>
             <p class="lead">2nd Edition</p>
         </div>
         <hr />
         <div class="row">
-            <div class="col-md-5 mb-3">
+            <!-- Left Side -->
+            <div class="col-md-4 mb-3">
                 <div class="text-center">
-                    <img src="../assets/img/af0ae5b4def62a32adfd5cf102c799a4.jpg"
-                        class="img-fluid shadow rounded profile" alt="Profile Picture" />
+                    <img src="../assets/img/joydebolle.jpg" class="img-fluid shadow rounded profile"
+                        alt="Profile Picture" />
                 </div>
-                <p class="text-muted m-0 text-end">- literally me</p>
-                <p class="m-0">art credit: <a href="https://x.com/ziriii_zireael/status/1830174794412871898"
-                        target="_blank">@ziriii_zireael</a></p>
+                <p class="text-muted m-0 text-end">- random art of mine</p>
                 <p class="m-0">contact me on discord: <a href="https://discord.com/" target="_blank">@nikigee</a></p>
                 <p>...or by email: <a href="mailto:ngolev.bus@gmail.com">ngolev.bus@gmail.com</a></p>
             </div>
+            <!-- Right side -->
             <div class="col fs-5">
+                <h2 class="h2 text-primary">About</h2>
                 <p>
                     Hey! You're looking at a digital player sheet I made for dnd 5th edition.
                 </p>
@@ -32,11 +33,11 @@
                     automation without being too bulky (plus, it's just fun to work on).
                 </p>
                 <p>
-                    It started out as a basic JS script I made back in 2018 and I've just slowly been adding to it with
-                    time -- This website being a total remake of that one, hence "2nd edition" :P
+                    Magic Dice started out as a basic JS script I made back in 2018 which then turned into a website --
+                    This version being a total remake of that one, hence "2nd edition" :P
                 </p>
                 <p>I hope it serves you well!</p>
-                <h3 class="h3 text-primary">Tips and Tricks</h3>
+                <h2 class="h2 text-primary">Tips and Tricks</h2>
                 <p class="text-muted fs-6">(random hidden features which may be of use to you)</p>
                 <ul>
                     <li>Typing "quickroll" into the dice tray followed by a diceroll e.g.
@@ -48,19 +49,29 @@
                     <li>Highly recommend you export your character frequently. No one wants to lose their hard work! I
                         put back-ups of all my characters in a cloud storage like OneDrive.</li>
                 </ul>
+                <h2 class="h2 text-primary">Credits</h2>
+                <p class="text-muted fs-6">A list of everything used in this project:</p>
+                <ul class="fs-6">
+                    <li>Logo design: <span class="text-primary">Alex Marshall</span></li>
+                    <li>Default banner art: <span class="text-primary">Spooky - Dmitry Zaviyalov</span></li>
+                    <li>Cyberpunk banner art: <span class="text-primary">Ghost in The Shell - Ash Thorp</span></li>
+                    <li>Art: <span class="text-primary">To camp - lok du</span></li>
+                    <li>404 Page Art: <span class="text-primary">denimcatfish</span></li>
+                    <li>Art: <span class="text-primary">HK 2040 - Gianluigi Di Giacomo</span></li>
+                    <li>Art: <span class="text-primary">Project War of Crown - Dora-G</span></li>
+                </ul>
             </div>
         </div>
-        <div class="text-muted py-3">
-            © 2025 Magic Dice
-        </div>
+        <Footerr />
 
     </div>
 </template>
 
 <script setup>
+import Footerr from "@/components/Footerr.vue";
 import { onMounted } from "vue";
 
-onMounted(()=>{
+onMounted(() => {
     document.title = "About - Magic Dice";
 })
 </script>
